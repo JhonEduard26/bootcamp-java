584cc03c84774516ada5c43481895316a054419d
Jhon Eduard Bocanegra <72460911+JhonEduard26@users.noreply.github.com>
2022-05-03 20:10:03 -0500

    Ejercicio 1

---
+++ sumNumbers.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[ ] args) {
+        System.out.println(sumNumbers(7, 10, 8));
+    }
+
+    public static Integer sumNumbers(int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+    }
+}
